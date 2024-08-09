78f07794995ceaee8af495f4572f9b900062fbd2
Pintu335 <pintukumarjnv335@gmail.com>
2024-08-09 15:53:55 +0530

    change in code

--- server/server.js
+++ server/server.js
@@ -51,6 +51,9 @@ server.post("/signup", (req,res) =>{
   })
 
   .catch(err=>{
+    if(err.code==11000){
+      return res.status(500).json({"error":"Email already exists"})
+    }
     return res.status(500).json({"error":err.message})
   })
 
